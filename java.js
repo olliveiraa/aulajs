68c108790cafb380f1d0d3cf0c3a3a71c9d4814d
rafael justen <rafafael.rj@gmail.com>
2019-09-20 13:48:27 -0300

    variáveis

---
+++ java.js
@@ -0,0 +1,19 @@
+let res;
+let num = 10;
+let boleano = true;
+
+console.log ("\nboolean para number");
+res = boleano -0
+console.log("resultado: ", res);
+console.log("tipo de variável: ", typeof res);
+
+console.log ("\nnumber para boolean");
+res  = !!num;
+console.log ("resultado: ", res)
+console.log("tipo de variável: ", typeof res);
+
+console.log("\nnumber para string");
+res = num + "";
+console.log("resultado: : ", res);
+console.log("tipo de variável: ", typeof res);
+
